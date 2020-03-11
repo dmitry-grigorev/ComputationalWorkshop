9344c19eb6fe1a8d5e90886066b0d13990e0c9f0
Дима <crafting20002033@gmail.com>
2020-03-11 10:14:16 +0300

    Task4 is practically done.

---
+++ Task4/module_for_task4/mft4.py
@@ -0,0 +1,34 @@
+from math import cos, pi
+from mft1_2 import get_values, get_ERs, get_Ns, get_Pks
+
+
+def equidistant_interpolation_Newton(f, pivot, a, b, degree, h):
+    values = get_values(f, a, b, h)
+
+    ERs = get_ERs(values, degree)
+
+    N = get_Ns(a, b, degree, h, pivot)
+
+    P, ER0 = get_Pks(a, b, degree, h, pivot, values, N, ERs)
+    print(P)
+    return P[len(P) - 1]
+
+
+def non_equidistant_interpolation_Newton(f, nodes, pivot, degree):
+    sorted_nodes = (sorted(nodes, key=lambda x: abs(x - pivot)))  # четыре ближайшие точки
+    DDs = get_DDs(f, nodes, degree)
+    value = 0
+    buf = 1
+    for i in range(degree):
+        value = value + DDs[i][0] * buf
+        buf *= (pivot - sorted_nodes[i])
+
+    return value
+
+
+def get_DDs(f, nodes, degree):
+    DDs = [[]] * degree
+    DDs[0] = [f(nodes[k]) for k in range(degree)]
+    for i in range(degree - 1):
+        DDs[i + 1] = ([(DDs[i][j + 1] - DDs[i][j]) / (nodes[i + j + 1] - nodes[j]) for j in range(degree - 1 - i)])
+    return DDs
