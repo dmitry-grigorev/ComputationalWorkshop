4fc11941fa0cb6a6c8e90714583d8ce22affae66
Дима <crafting20002033@gmail.com>
2020-03-19 21:42:44 +0300

    Code is commented.

--- Task5/module_for_task5/mft5.py
+++ Task5/module_for_task5/mft5.py
@@ -2,8 +2,8 @@ from mft4 import non_equidistant_interpolation_Newton
 from pandas import DataFrame
 
 
-def derivate_experiment(derivative, f, h, pivot, nodes, df):
-    values = []
+def derivate_experiment(derivative, f, h, pivot, nodes, df):  # Функция, вычисляющая оптимальный шаг экспериментально
+    values = []  # Это будут списки значений и фактических погрешностей
     errors = []
     h_s = [h]
     values.append(derivative(f, h, pivot, nodes))
@@ -22,17 +22,18 @@ def derivate_experiment(derivative, f, h, pivot, nodes, df):
     return DataFrame([h_s, values, errors], index=["$h$", "Численное значение производной", "Погрешность"])
 
 
-def get_table(f, nodes, degree, h, df, d2f):
-    values_in_nodes = [f(x) for x in nodes]
+def get_table(f, nodes, degree, h, df, d2f):  # Строит таблицу по образцу
+    values_in_nodes = [f(x) for x in nodes]  # Списки значений функции и её производных, точные
     real_val_der1 = [df(x) for x in nodes]
     real_val_der2 = [d2f(x) for x in nodes]
-    num_val_der1_1 = [(values_in_nodes[i + 1] - values_in_nodes[i]) / h for i in range(degree)]
+    num_val_der1_1 = [(values_in_nodes[i + 1] - values_in_nodes[i]) / h for i in
+                      range(degree)]  # Списки значений функции и её производных, приближенные
     num_val_der1_1 += [num_val_der1_1[degree - 1]]
     num_val_der1_2 = [(-3 * values_in_nodes[0] + 4 * values_in_nodes[1] - values_in_nodes[2]) / (2 * h)] + \
                      [(values_in_nodes[i + 1] - values_in_nodes[i - 1]) / (2 * h) for i in range(1, degree)] + \
                      [(3 * values_in_nodes[degree] - 4 * values_in_nodes[degree - 1] + values_in_nodes[degree - 2]) / (
                              2 * h)]
-    errors1_1 = [real_val_der1[i] - num_val_der1_1[i] for i in range(degree + 1)]
+    errors1_1 = [real_val_der1[i] - num_val_der1_1[i] for i in range(degree + 1)]  # Списки погрешностей
     errors1_2 = [real_val_der1[i] - num_val_der1_2[i] for i in range(degree + 1)]
     num_val_der2 = [''] + [(values_in_nodes[i + 1] - 2 * values_in_nodes[i] + values_in_nodes[i - 1]) / (h ** 2) for i
                            in range(1, degree)] + ['']
